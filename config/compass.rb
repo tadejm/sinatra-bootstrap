5c04876c2669f67b2917fdc643c5217f7fb15da5
Tadej Murovec <tadej.murovec@gmail.com>
2010-07-31 11:52:31 +0200

    removed duplicated configuration

--- config/compass.rb
+++ config/compass.rb
@@ -1,7 +1,4 @@
 require 'susy'
-
-# Require any additional compass plugins here.
-
 if defined?(Sinatra)
   # This is the configuration to use when running within sinatra
   project_path = Sinatra::Application.root
@@ -13,14 +10,11 @@ else
   environment = :production
 end
 
-# Set this to the root of your project when deployed:
+# This is common configuration
+sass_dir = File.join 'views', 'stylesheets'
+images_dir = File.join 'static', 'images'
 http_path = "/"
-css_dir = "static/stylesheets"
-http_stylesheets_path = "/stylesheets"
-sass_dir = "views/stylesheets"
-images_dir = "static/images"
 http_images_path = "/images"
-javascripts_dir = "javascripts"
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-relative_assets = true
+http_stylesheets_path = "/stylesheets"
+javascripts_dir = "/javascripts"
+relative_assets = false
